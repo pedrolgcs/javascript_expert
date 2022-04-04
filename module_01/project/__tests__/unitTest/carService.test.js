3363b5fe7d83a176a82bcf50e8fd515347d7ccec
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-04-04 15:05:23 -0300

    create rent method and test fro him

--- module_01/project/__tests__/unitTest/carService.test.js
+++ module_01/project/__tests__/unitTest/carService.test.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const { describe, it, before, beforeEach, afterEach } = require('mocha');
 const CarService = require('../../src/services/carService');
+const Transaction = require('../../src/entities/transaction');
 
 const CARS_DATABASE = path.join(
   __dirname,
@@ -71,5 +72,58 @@ describe('CarService', () => {
     expect(sut).to.be.deep.equal(expected);
     expect(carService.chooseRandomCar.calledOnce).to.be.ok;
     expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok;
+  });
+
+  it('should be able to calculate final amount in real', async () => {
+    const customer = Object.create(MOCKS.customer);
+    customer.age = 50;
+    const carCategory = Object.create(MOCKS.carCategory);
+    carCategory.price = 37.6;
+    const numberOfDays = 5;
+    const expected = carService.currencyFormat.format(244.4);
+
+    sandbox
+      .stub(carService, 'taxesBaseOnAge')
+      .get(() => [{ from: 40, to: 50, then: 1.3 }]);
+
+    const sut = carService.calculateFinalPrice(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+
+    expect(sut).to.be.equal(expected);
+  });
+
+  it('should be able to given a car and a car category and return transaction', async () => {
+    const car = Object.create(MOCKS.car);
+    const customer = {
+      ...MOCKS.customer,
+      age: 20,
+    };
+    const carCategory = {
+      ...MOCKS.carCategory,
+      price: 37.6,
+      carIds: [car.id],
+    };
+    const numberOfDays = 5;
+    const mockNow = new Date(2020, 10, 5);
+
+    sandbox.useFakeTimers(mockNow.getTime());
+    sandbox
+      .stub(carService.carRepository, carService.carRepository.find.name)
+      .resolves(car);
+
+    const expectedAmount = carService.currencyFormat.format(206.8);
+    const expected = new Transaction({
+      customer,
+      car,
+      dueDate: '10 de novembro de 2020',
+      amount: expectedAmount,
+    });
+
+    const sut = await carService.rent(customer, carCategory, numberOfDays);
+
+    expect(sut).to.be.deep.equal(expected);
   });
 });
--- module_01/project/src/services/carService.js
+++ module_01/project/src/services/carService.js
@@ -1,10 +1,19 @@
 const BaseRepository = require('../repositories/baseRepository');
+const Tax = require('../entities/tax');
+const Transaction = require('../entities/transaction');
 
 class CarService {
   carRepository;
+  currencyFormat;
+  taxesBaseOnAge;
 
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars });
+    this.taxesBaseOnAge = Tax.taxesBaseOnAge;
+    this.currencyFormat = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
   }
 
   getRandomPositionFromArray(list) {
@@ -21,6 +30,46 @@ class CarService {
     const carId = this.chooseRandomCar(carCategory);
     const car = await this.carRepository.find(carId);
     return car;
+  }
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBaseOnAge.find(
+      (tax) => age >= tax.from && age <= tax.to
+    );
+
+    const finalPrice = tax * price * numberOfDays;
+
+    return this.currencyFormat.format(finalPrice);
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.getAvailableCar(carCategory);
+
+    const finalPrice = this.calculateFinalPrice(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+
+    const dueDate = new Date();
+    dueDate.setDate(dueDate.getDate() + numberOfDays);
+
+    const formattedDueDate = dueDate.toLocaleDateString('pt-BR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    const transaction = new Transaction({
+      customer,
+      dueDate: formattedDueDate,
+      car,
+      amount: finalPrice,
+    });
+
+    return transaction;
   }
 }
 
