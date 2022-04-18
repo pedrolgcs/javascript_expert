ce0e94610231be9c565c7cfbff8f513a52a06bb4
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-04-18 07:48:50 -0300

    use app root path

--- module_03/project/src/repositories/personRepository.js
+++ module_03/project/src/repositories/personRepository.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
+import appRoot from 'app-root-path';
 
 export const save = async (data) => {
+  /*
+  ! Not working with mocha
   const { pathname: databasePath } = new URL(
     '../infra/database/db.json',
     import.meta.url
   );
+  */
+
+  const databasePath = appRoot.resolve('src/infra/database/db.json');
 
   const currentData = JSON.parse(fs.readFileSync(databasePath, 'utf8'));
 
