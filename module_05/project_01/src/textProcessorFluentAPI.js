297af6460b7c998129cf44ea6400843beb0f91cb
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-04-29 11:23:38 -0300

    add regex description

--- module_05/project_01/src/textProcessorFluentAPI.js
+++ module_05/project_01/src/textProcessorFluentAPI.js
@@ -6,8 +6,19 @@ class TextProcessorFluentAPI {
   }
 
   extractPeopleData() {
+    /*
+      ?<= fala que vai extrair os dados que virão depois
+      [contratante|contratante] ou um ou outro
+      :\s{1} vai procurar os : seguindo de um espaço
+      tudo fica dentro de um grupo
+
+      (?!s) negative look around, vai ignorar os contrantes no fim do documento
+      .*\n vai pegar tudo até a quebra de linha
+      .*? faz com que pare na primeira recorrência
+    */
     const matchPerson =
-      /(?<=[Contratante|Contratada]:\s{1})(?!\s)(.*\n.*?)$/gim;
+      /(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gim;
+
     const onlyPersons = this.#content.match(matchPerson);
 
     this.#content = onlyPersons;
